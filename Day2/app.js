6ccc9e5c6fab20cc75b65003c48d636828d52a29
Jzridar <jzridar@gmail.com>
2023-03-01 20:46:31 -0500

    Adding app file

---
+++ Day2/app.js
@@ -0,0 +1,67 @@
+x = 5
+if (x==10){
+    console.log(true)
+
+} else
+{
+console.log(false)
+}
+
+// var num = -7
+// if (num >= 0)
+// {
+//     console.log("positive")
+// } else {
+//     console.log("negative")
+// }
+
+// var age=17
+// if (age >=18)
+// {
+//     console.log("eligible")
+// } else {
+//     console.log("not eligible")
+// }
+
+
+
+//Nested if else statement
+// y=25
+
+// if(y<=100)
+//     {// has to be true to go into the loop else it exits to the last else
+//     //Do this
+// } else{ if (y%2==0)
+// //do this
+// }
+// } else{
+// //do this
+// }
+
+//LOOps
+// for(let i=0; i<=10;i++){ //(initializer;conditional stratement; iterator)
+//     console.log(i)
+// }
+
+// for (let i=6; i<=60 ;i+=3){
+// console.log(i) }
+
+//console.log("Hello Jaya")
+
+//Arrays
+//let nums =[2,4,18]
+
+// let movies =["Home alone","Titanic","Evil dead"]
+// //let firstmovie = movies[0]
+// console.log(movies[2])
+// console.log(movies.push("tamil","padam"))
+// console.log(movies)
+// console.log(movies.push.unshift("the lego","padam"))
+// console.log(movies)
+// console.log(movies.pop())
+// console.log(movies)
+// console.log(movies.shift())
+
+// movies.forEach(function(movie) {
+// 	console.log(movie);
+// });
